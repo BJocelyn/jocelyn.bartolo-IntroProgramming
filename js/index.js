9edd29a59e2bdaa34719f4c63be355ff439ae531
BJocelyn <jocelyn.bartolo@outlook.com>
2026-06-01 23:34:38 -0400

    fix: correct footer by adding today variable and createElement

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 const body = document.querySelector("body");
 const footer = document.createElement("footer");
-const thisYear = new Date().getFullYear();
-footer.innerHTML = `
+const today = new Date();
+const thisYear = today.getFullYear();
+/* const thisYear = new Date().getFullYear(); */
+const copyright = document.createElement("p");
+copyright.innerHTML = `Copyright &copy; ${thisYear} Jocelyn Bartolo. All rights reserved.`;
+/* footer.innerHTML = `
   <p>Copyright &copy; ${thisYear} Jocelyn Bartolo. All rights reserved.</p>
 `;
+body.appendChild(footer); */
+footer.appendChild(copyright);
 body.appendChild(footer);
-
 const skills = ["JavaScript", "HTML", "CSS", "Git & GitHub", "SolidWorks", "AutoCAD", "Autodesk Inventor"];
 const skillsSection = document.getElementById("skills");
 const skillsList = skillsSection.querySelector("ul");
